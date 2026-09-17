938cae37665f63361a9c865c47e5ba86bc7e2239
FraPanca <francescopancaldi075@gmail.com>
2026-09-17 17:57:04 +0200

    Fix Helm provider v2.x block syntax
    The kubernetes/exec/set arguments were written using v3.x's object-assignment syntax, incompatible with the pinned ~> 2.13 (v2.x, SDK-based) provider, which still uses classic HCL blocks.

--- terraform-aws-eks/kubernetes.tf
+++ terraform-aws-eks/kubernetes.tf
@@ -10,11 +10,11 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
 
-    exec = {
+    exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.name, "--region", var.aws_region]
@@ -29,10 +29,10 @@ resource "helm_release" "ingress_nginx" {
   namespace        = "ingress-nginx"
   create_namespace = true
 
-  set = [{
+  set {
     name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
     value = "nlb"
-  }]
+  }
 
   depends_on = [aws_eks_node_group.default]
 }
