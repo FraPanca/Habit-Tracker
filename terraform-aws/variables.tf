370b09ca50c040a1f67e4dc5be721d102ca7ca34
FraPanca <francescopancaldi075@gmail.com>
2026-09-17 17:04:27 +0200

    Fix OIDC trust policy for GitHub's immutable subject claim format
    Repos created after July 15, 2026 use repo:OWNER@OWNER_ID/REPO@REPO_ID in the sub claim instead of just names. Updates the StringLike condition accordingly.

--- terraform-aws/variables.tf
+++ terraform-aws/variables.tf
@@ -54,3 +54,15 @@ variable "github_repo" {
   type        = string
   default     = "FraPanca/Habit-Tracker"
 }
+
+variable "github_owner_id" {
+  description = "ID numerico immutabile dell'account GitHub, usato nel claim OIDC 'sub'"
+  type        = string
+  default     = "195170957"
+}
+
+variable "github_repository_id" {
+  description = "ID numerico immutabile del repository GitHub, usato nel claim OIDC 'sub'"
+  type        = string
+  default     = "1340543158"
+}
