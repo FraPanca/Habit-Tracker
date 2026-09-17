66ce7b76a2b6b3cefa1ad890cb1a236abb05d2b5
FraPanca <francescopancaldi075@gmail.com>
2026-09-17 17:52:46 +0200

    Install ingress-nginx via Terraform-managed Helm release
    Configures the kubernetes/helm providers against the new EKS cluster using IAM-based exec auth (aws eks get-token), and deploys ingress-nginx with the NLB annotation instead of the default (legacy, deprecated) Classic Load Balancer.

---
+++ terraform-aws-eks/kubernetes.tf
@@ -0,0 +1,38 @@
+provider "kubernetes" {
+  host                   = aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.name, "--region", var.aws_region]
+  }
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.name, "--region", var.aws_region]
+    }
+  }
+}
+
+resource "helm_release" "ingress_nginx" {
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-nginx"
+  create_namespace = true
+
+  set = [{
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+    value = "nlb"
+  }]
+
+  depends_on = [aws_eks_node_group.default]
+}
